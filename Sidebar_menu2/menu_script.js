d0fd5361ea88f817f0dee107822083bf0250e6c0
Tobiasz Wójcik <tbaztw@gmail.com>
2023-01-08 01:26:49 +0100

    template with moveing sidebare menu on left

---
+++ Sidebar_menu2/menu_script.js
@@ -0,0 +1,5 @@
+menuButton = function(){
+    document.getElementById("menu_button").classList.toggle("change");
+    document.getElementById("sidebar-menu").classList.toggle("change");
+    document.querySelector("nav").classList.toggle("nav_opacity");
+}
